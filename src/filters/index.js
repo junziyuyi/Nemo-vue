340a9b39f103c268d294241dd29a24b4d842e170
tanyp <759227027@qq.com>
2019-04-18 23:24:56 +0800

    feat：初始化项目

--- src/filters/index.js
+++ src/filters/index.js
@@ -7,3 +7,14 @@ export function formatCurrency(num) {
     .toString()
     .replace(/^-?\d+/g, m => m.replace(/(?=(?!\b)(\d{3})+$)/g, ","));
 }
+/**
+ * @param {number} num
+ */
+exports.formatMoment = (data, format = "YYYY-MM-DD") => {
+  let v = window.$Vue;
+  if (data) {
+    return v.moment(data).format(format);
+  } else {
+    return "";
+  }
+};
