0be268198bed9b69c2206b88e9a96f45fa351b79
tanyp09 <tanyinping@users.noreply.github.com>
2019-04-18 19:08:09 +0800

    feat：初始化项目

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from "vue";
+import Element from "element-ui";
 import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import router from "./router/index.js";
+import store from "./store/index";
+
+Vue.use(Element);
 
 Vue.config.productionTip = false;
 
