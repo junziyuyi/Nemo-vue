369efba46394ebaeda41b0d6c12bbbac33a499cd
wangzhizhong <18654367@qq.com>
2019-04-30 17:28:37 +0800

    chore:添加进度条插件nprogress  修改eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,12 +6,13 @@ module.exports = {
   extends: ["plugin:vue/essential", "@vue/prettier"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "prettier/prettier": "off"
   },
   parserOptions: {
     parser: "babel-eslint",
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     }
   }
 };
